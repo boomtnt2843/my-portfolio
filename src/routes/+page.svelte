baaa1a6a308122893c8fd2bd79f26d0f358c7ce9
boomtnt2843 <boomphat@odds.team>
2023-11-02 14:58:45 +0700

    init route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
-
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-</style>
+<section class="flex w-screen h-screen justify-center items-center">
+  <div class="flex flex-col w-fit px-4">
+    <h1 class="text-6xl font-bold">Hello world!</h1>
+    <div class="flex justify-around my-2">
+      <a href="/work">
+        <button>💼 Work</button>
+      </a>
+      <a href="/hobby">
+        <button>🎯 Hobby</button>
+      </a>
+    </div>
+  </div>  
+</section>
