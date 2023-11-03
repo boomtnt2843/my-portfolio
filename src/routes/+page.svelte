82d1d27c1946f37f13e2fcc343599e614a19bb8b
boomtnt2843 <boomphat@odds.team>
2023-11-03 17:48:15 +0700

    comment route button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,15 +4,15 @@
 
 <section class="flex w-screen h-screen justify-center items-end">
   <div class="flex flex-col w-fit px-4 mb-10 items-center justify-end space-y-4">
-    <h1 class="text-6xl font-bold">Phattarapong Pimhom | boom</h1>
-    <div class="flex justify-around w-full my-2">
+    <h1 class="text-[6vmin] font-bold">Phattarapong Pimhom | boom</h1>
+    <!-- <div class="flex justify-around w-full my-2">
       <a href="/work">
-        <button>💼 Work</button>
+        <button class="text-[3vmin] ">💼 Work</button>
       </a>
       <a href="/hobby">
-        <button>🎯 Hobby</button>
+        <button class="text-[3vmin]">🎯 Hobby</button>
       </a>
-    </div>
+    </div> -->
     <h2 class="text-4xl font-bold"> 🚧 develop Soon. . .</h2>
     <img class="w-64 h-64" src={saynumProfile} alt="pf">
   </div>  
