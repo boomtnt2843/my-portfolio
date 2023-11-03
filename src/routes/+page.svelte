a106d098f6fb024ea7c55b5c22a3d026fa688c1d
boomtnt2843 <boomphat@odds.team>
2023-11-03 13:31:29 +0700

    add img profile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,6 @@
-<section class="flex w-screen h-screen justify-center items-center">
-  <div class="flex flex-col w-fit px-4 items-center space-y-2">
-    <img class=" w-64 h-64" src="src/lib/image/saynum_pf.jpeg" alt="pf">
+<section class="flex w-screen h-screen justify-center items-end">
+  <div class="flex flex-col w-fit px-4 mb-10 items-center justify-end space-y-4">
     <h1 class="text-6xl font-bold">Phattarapong Pimhom | boom</h1>
-    <h2 class="text-4xl font-bold"> 🚧 Soon. . .</h2>
     <div class="flex justify-around w-full my-2">
       <a href="/work">
         <button>💼 Work</button>
@@ -11,5 +9,7 @@
         <button>🎯 Hobby</button>
       </a>
     </div>
+    <h2 class="text-4xl font-bold"> 🚧 develop Soon. . .</h2>
+    <img class="w-64 h-64" src="src/lib/image/saynum_pf_no_bg.png" alt="pf">
   </div>  
 </section>
